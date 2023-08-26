794d689db042b2247fbaccee145fc15f2927f295
Dương Tuấn Anh <duongtuananh2211@gmail.com>
2023-08-26 18:10:01 +0700

    [KAKEHI-1] Restyle floatting bottom buttons

--- src/app/components/FloatingButtons/FloatingButtons.tsx
+++ src/app/components/FloatingButtons/FloatingButtons.tsx
@@ -18,11 +18,11 @@ const FloatingButtons: React.FC<IProps> = () => {
             <img src="/icons/chevron-circle-icon.png" alt="Chevron up circle icon" />
         </button>
         <div className='floating-buttons grid grid-cols-6 w-full'>
-            <a className='email-button w-full flex flex-col items-center justify-center'>
+            <a className='email-button w-full flex flex-col items-center justify-around'>
                 <img src="/icons/email-icon.png " className='btn-icon inline' alt='Email icon' />
                 <p>お問い合わせ</p>
             </a>
-            <a className='document-button w-full flex flex-col items-center justify-center'>
+            <a className='document-button w-full flex flex-col items-center justify-around'>
                 <img src="/icons/document-icon.png" className='btn-icon inline' alt='Document icon' />
                 <p>資料請求</p>
             </a>
