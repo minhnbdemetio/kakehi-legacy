e41f4be3b41344a30ca062da48c6763699c2a741
Dương Tuấn Anh <duongtuananh2211@gmail.com>
2023-08-28 16:58:12 +0700

    [KAKEHI-2] Moving shared components to layout components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import Header from "./components/Header/Header";
 import BackgroundWithSlogan from "./components/BackgroundWithSlogan";
-import FloatingButtons from "./components/FloatingButtons";
-import MobileSidebar from "./components/MobileSidebar";
-import PageTitle from "./components/PageTitle";
 import TopPageInformation from "./organisms/TopPageInformation";
 import TopPageWorkSection from "./organisms/TopPageWorkSection";
 import TopPageFLowSection from "./organisms/TopPageFLowSection/TopPageFLowSection";
-import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <BackgroundWithSlogan src="/index-background.jpg" >
         <h1 className="text-3xl">システム建築で、</h1>
         <h1 className="text-3xl">工場・倉庫に</h1>
@@ -28,13 +22,7 @@ export default function Home() {
         <TopPageFLowSection />
       </div>
 
-      <Footer />
 
-      <FloatingButtons />
-
-
-
-      <MobileSidebar />
     </>
   );
 }
