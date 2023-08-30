29eee3f982b77a550e600fe122ed150ac8dcf887
Dương Tuấn Anh <duongtuananh2211@gmail.com>
2023-08-30 13:01:44 +0700

    [KAKEHI-3] Add sample projects page title

---
+++ src/app/work/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SectionTitle from "../components/SectionTitle";
+
+
+interface IProps { }
+
+const Work: React.FC<IProps> = () => {
+
+    return <div>
+        <SectionTitle position="left" subtitle="work" fistTitle="施工事例" secondTitle="株式会社さんれいフーズ" />
+    </div>
+}
+
+export default Work;
