25add2db1c7db03613f4a0b621b5b2f4eab64774
maidt <doantuyetmai2000@gmail.com>
2023-09-11 21:48:43 +0700

    [KAKEHI-19] display saved data in form

--- src/app/components/RequestForm/RequestForm.tsx
+++ src/app/components/RequestForm/RequestForm.tsx
@@ -25,7 +25,7 @@ const schema = Yup.object().shape({
     .email(formValidationMessage.INVALID_EMAIL),
   emailConfirm: Yup.string()
     .required(formValidationMessage.REQUIRED)
-    .email(formValidationMessage.INVALID_CONFIRMATION_EMAIL)
+    .email(formValidationMessage.INVALID_EMAIL)
     .equals([Yup.ref("email")], formValidationMessage.PRIVACY_ACCEPT_REQUIRED),
   phoneNumber: (Yup.string() as any)
     .required(formValidationMessage.REQUIRED)
