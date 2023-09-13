afc79dc257ab812fed01a52a92093659994051f3
maidt <doantuyetmai2000@gmail.com>
2023-09-13 22:23:16 +0700

    [KAKEHI-31] Fix bugs profile

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -11,7 +11,7 @@ const Profile: React.FC<IProps> = () => {
   return (
     <div className="profile pb-22 xl:pb-38.1/3">
       <BackgroundWithSlogan src="/company-profile-background.png">
-        <h1 className="text-3xl">会社概要</h1>
+        <h1 className="xl:text-3.5xl text-3xl leading-snug">会社概要</h1>
       </BackgroundWithSlogan>
 
       <ul className="mx-7 mt-22 grid grid-cols-1 gap-3 xl:mx-pc-container xl:mt-33.1/3 xl:gap-7">
@@ -22,6 +22,8 @@ const Profile: React.FC<IProps> = () => {
               background="card-primary"
               label={item.label}
               description={item.description}
+              descriptionClassName="xl:text-xl xl:font-normal xl:leading-loose"
+              labelClassName="xl:text-xl xl:font-bold xl:leading-snug"
             />
           </li>
         ))}
