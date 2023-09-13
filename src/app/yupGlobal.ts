304c42695e3a42901b60e289e090e393e798fe7c
maidt <doantuyetmai2000@gmail.com>
2023-09-13 22:15:15 +0700

    [KAKEHI-18] Update jpPhone validation

--- src/app/yupGlobal.ts
+++ src/app/yupGlobal.ts
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 
 Yup.addMethod(Yup.string, "jpPhone", function (message) {
-  const JP_PHONE_REGEX = /^\d{2}(?:-\d{4}-\d{4}|\d{8}|\d-\d{3,4}-\d{4})$/;
+  const JP_PHONE_REGEX = /^\d{2,3}(?:-\d{4}-\d{4}|\d{8}|\d-\d{3,4}-\d{4})$/;
   return this.matches(JP_PHONE_REGEX, { message, excludeEmptyString: true });
 });
 
