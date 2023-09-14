40654d0ee44510c2dc5b220b00b00d54234a3ebd
maidt <doantuyetmai2000@gmail.com>
2023-09-14 21:43:26 +0700

    [KAKEHI-31] fix bugs profile

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -10,8 +10,13 @@ interface IProps {}
 const Profile: React.FC<IProps> = () => {
   return (
     <div className="profile pb-22 xl:pb-38.1/3">
-      <BackgroundWithSlogan src="/company-profile-background.png">
-        <h1 className="xl:text-3.5xl text-3xl leading-snug">会社概要</h1>
+      <BackgroundWithSlogan
+        src="/company-profile-background.png"
+        className="xl:!h-[544px]"
+      >
+        <h1 className="leading-snug text-3xl font-bold xl:text-3.5xl">
+          会社概要
+        </h1>
       </BackgroundWithSlogan>
 
       <ul className="mx-7 mt-22 grid grid-cols-1 gap-3 xl:mx-pc-container xl:mt-33.1/3 xl:gap-7">
@@ -22,8 +27,8 @@ const Profile: React.FC<IProps> = () => {
               background="card-primary"
               label={item.label}
               description={item.description}
-              descriptionClassName="xl:!text-xl xl:!font-normal xl:!leading-loose"
-              labelClassName="xl:!text-xl xl:!font-bold xl:!leading-snug"
+              descriptionClassName="!px-9 xl:!px-7 xl:!text-xl xl:!font-normal xl:!leading-[2] xl:!justify-start"
+              labelClassName="xl:!text-xl xl:!font-bold xl:!leading-[1.44]"
             />
           </li>
         ))}
