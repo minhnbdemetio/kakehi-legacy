233029f08a6211dc214b4842a99865c52e0d155b
Dương Tuấn Anh <duongtuananh2211@gmail.com>
2023-09-29 22:29:14 +0700

    [KAKEHI-3] Fix ui bugs

--- src/app/organisms/TopPageWorkSection/TopPageWorkSection.tsx
+++ src/app/organisms/TopPageWorkSection/TopPageWorkSection.tsx
@@ -10,12 +10,12 @@ const TopPageWorkSection: React.FC<IProps> = async () => {
 
   return (
     <div>
-      <div className="mt-[50px] flex flex-col items-center justify-center">
+      <div className="mt-[50px] flex flex-col items-center justify-center xl:mt-[95px]">
         <div className="mb-[5px] w-max border-b-2 border-color-custom pb-[3px] text-center font-noto-sans text-xl uppercase text-primary xl:px-0">
           WORK
         </div>
 
-        <div className="mb-[72px] font-noto-sans-jp text-2xl font-bold xl:mb-[40px] xl:px-0 xl:text-3xl">
+        <div className="mb-[72px] font-noto-sans-jp text-2xl font-bold xl:mb-[110px] xl:px-0 xl:text-3xl">
           施工実績
         </div>
       </div>
