47fa45acd619cd6c528dd51789e06d57c1e8597a
Dương Tuấn Anh <duongtuananh2211@gmail.com>
2023-10-09 10:07:08 +0700

    [KAKEHI-36] Add top page slider

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,18 +1,20 @@
-import BackgroundWithSlogan from "../components/BackgroundWithSlogan";
-import TopPageInformation from "../organisms/TopPageInformation";
-import TopPageWorkSection from "../organisms/TopPageWorkSection";
-import TopPageFLowSection from "../organisms/TopPageFLowSection/TopPageFLowSection";
-import TopSectionWork1 from "../components/TopSectionWork1";
-import TopSectionWork2 from "../components/TopSectionWork2";
-import TopSectionWork3 from "../components/TopSectionWork3";
+import TopPageInformation from "@/app/organisms/TopPageInformation";
+import TopPageWorkSection from "@/app/organisms/TopPageWorkSection";
+import TopPageFLowSection from "@/app/organisms/TopPageFLowSection/TopPageFLowSection";
+import TopSectionWork1 from "@/app/components/TopSectionWork1";
+import TopSectionWork2 from "@/app/components/TopSectionWork2";
+import TopSectionWork3 from "@/app/components/TopSectionWork3";
+import SliderBackgroundWithSlogan from "@/app/components/SliderBackgroundWithSlogan";
 
 export default function Home() {
   return (
     <>
-      <BackgroundWithSlogan src="/index-background.jpg">
-        <h1 className="hidden whitespace-pre-wrap text-3xl xl:block">{`システム建築で、\n工場・倉庫に 最適な答えを。`}</h1>
-        <h1 className="block whitespace-pre-wrap text-3xl xl:hidden">{`システム建築で、\n工場・倉庫に \n最適な答えを。`}</h1>
-      </BackgroundWithSlogan>
+      <SliderBackgroundWithSlogan
+        images={["/index-background.jpg", "/company-profile-background.png"]}
+      >
+        <h1 className="hidden whitespace-pre-wrap text-center text-3xl xl:block">{`システム建築で、\n工場・倉庫に 最適な答えを。`}</h1>
+        <h1 className="block whitespace-pre-wrap text-center text-3xl xl:hidden">{`システム建築で、\n工場・倉庫に \n最適な答えを。`}</h1>
+      </SliderBackgroundWithSlogan>
 
       <TopPageInformation />
 
@@ -24,7 +26,7 @@ export default function Home() {
 
       <TopPageWorkSection />
 
-      <TopPageFLowSection />
+      {/* <TopPageFLowSection /> */}
     </>
   );
 }
