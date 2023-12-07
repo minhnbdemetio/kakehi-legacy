81c4bf009ba0d0e8e55206f63b2b2333dd34140a
Dương Tuấn Anh <anhdt@demetio.com>
2023-12-07 03:45:59 +0000

    Develop

--- src/app/organisms/TopPageFLowSection/items.ts
+++ src/app/organisms/TopPageFLowSection/items.ts
@@ -2,54 +2,63 @@ const items: {
   img: string;
   label: string;
   content: string;
+  bgColor: string;
 }[] = [
   {
     label: "お問い合わせ",
-    img: "/flows/flow-1.png",
+    img: "/flows/kakehi_system_lp_flow_icon-1.png",
     content:
       "工場や倉庫などの建物の建築に関するお悩みやご要望を、お問い合わせフォームよりご連絡ください。",
+    bgColor: "#dcbec7",
   },
   {
     label: "ヒアリング",
-    img: "/flows/flow-2.png",
+    img: "/flows/kakehi_system_lp_flow_icon-2.png",
     content:
       "お客様の事業構想やご要望を詳細にお伺いするためにお打ち合わせを行います。ご相談内容に基づき、最善な解決方法をご提案いたします。",
+    bgColor: "#cca4b0",
   },
   {
     label: `基本計画・\n概算見積り`,
-    img: "/flows/flow-3.png",
+    img: "/flows/kakehi_system_lp_flow_icon-3.png",
     content:
       "基本平面プランと外観プランをご提案いたします。お客様との完成イメージの共有後に、概算見積りをご提示させていただきます。",
+    bgColor: "#b98291",
   },
   {
     label: `詳細設計・\n調査`,
-    img: "/flows/flow-4.png",
+    img: "/flows/kakehi_system_lp_flow_icon-4.png",
     content:
       "構造設計や設備設計などの詳細設計を作成し、ボーリング調査や法的チェックを行います。当工程より一部料金が発生いたします。",
+    bgColor: "#aa677c",
   },
   {
     label: "本見積り",
-    img: "/flows/flow-5.png",
+    img: "/flows/kakehi_system_lp_flow_icon-5.png",
     content:
       "詳細設計の内容にご理解いただき、プランが確定しましたら、本見積りをご提示させていただきます。",
+    bgColor: "#9f5369",
   },
   {
     label: `ご契約・\n着工`,
-    img: "/flows/flow-6.png",
+    img: "/flows/kakehi_system_lp_flow_icon-6.png",
     content:
       "ご契約の締結後に、資材発注をはじめとする必要な手配を開始いたします。近隣施設や住民の方々へのご挨拶を行い、着工となります。",
+    bgColor: "#913a54",
   },
   {
     label: `竣工・\n検査`,
-    img: "/flows/flow-7.png",
+    img: "/flows/kakehi_system_lp_flow_icon-7.png",
     content:
       "建物が完成しましたら、社内検査と法定検査を行います。試運転を実施後、お客様に検査を行っていただきます。",
+    bgColor: "#7f1e3d",
   },
   {
     label: "お引き渡し",
-    img: "/flows/flow-8.png",
+    img: "/flows/kakehi_system_lp_flow_icon-8.png",
     content:
       "お客様の検査が完了しましたら、お引き渡しとなります。建物の使用や管理に関するお取扱いにつきましてご説明いたします。",
+    bgColor: "#831f3f",
   },
 ];
 
